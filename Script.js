71b706a9671a2119931e3a57d6fe17384efe8ac2
rz-electronicshub <rzelectronicshub3027@gmail.com>
2026-08-11 12:27:29 +0530

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,12 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', e => {
+    const target = document.querySelector(link.getAttribute('href'));
+
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
